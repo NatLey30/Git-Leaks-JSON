f98b85adc7b6441e91a7a9bb01f0aa91f5a9c362
NatLey30 <112613481+NatLey30@users.noreply.github.com>
2022-11-03 09:40:21 +0100

    .

--- git_leaks.py
+++ git_leaks.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from git import Repo
 import re, signal, sys, time, pwn, pdb, os
+import json
 
 def handler_signal(signal, frame):
     print('\n\n[!] Out ..........\n')
@@ -12,16 +13,19 @@ def extract(url):
     return commits
 
 def transform(commits, KEY_WORDS):
-    coincidencias = []
+    coincidencias = {}
     for commit in commits:
         for word in KEY_WORDS:
             if re.search(word, commit.message, re.IGNORECASE):
-                coincidencias.append(str(f'Commit: {commit.hexsha} -> {commit.message}'))
+                coincidencias[str(commit.hexsha)] = str(commit.message)
+                #coincidencias.append(str(f'Commit: {commit.hexsha} -> {commit.message}'))
     return coincidencias
 
 def load(coincidencias):
-    for i in coincidencias:
-        print(i)
+    string = json.dumps(coincidencias)
+    file = open("leaks.json",'w')
+    file.write(string)
+    file.close()
 
 if __name__ == '__main__':
 
